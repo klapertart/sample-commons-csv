1fc97ce00c76ee27b8a563ab053b87d028484ffc
otong.sunandar <otong.sunandar@tritronik.com>
2024-08-23 10:29:39 +0700

    sample write file

---
+++ src/test/java/klapertart/lab/commonscsv/WriteFileTest.java
@@ -0,0 +1,37 @@
+package klapertart.lab.commonscsv;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author tritronik
+ * @since 8/23/2024
+ */
+
+@SpringBootTest
+@Slf4j
+public class WriteFileTest {
+
+    @Test
+    void testCreateCsvFile()throws IOException {
+        String filePath = "output.csv";
+        FileWriter writer = new FileWriter(filePath);
+
+        String[] headers = { "ID", "Name", "Age", "Country" };
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(headers));
+
+        csvPrinter.printRecord(1, "Ari", 10, "Indonesia");
+        csvPrinter.printRecord(2, "Budi", 11, "Indonesia");
+        csvPrinter.printRecord(3, "Caca", 10, "Indonesia");
+
+        csvPrinter.flush();
+
+        System.out.println("CSV file created successfully: " + filePath);
+    }
+}
